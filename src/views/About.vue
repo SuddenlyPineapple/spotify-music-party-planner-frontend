1e06144dbb92c2d111db7dfc97a5cc1aad11d77e
Wojciech Kasperski <wojciech.kasperski97@gmail.com>
2020-03-23 18:08:24 +0100

    UserStory4-Add_Event_Page

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,6 +3,27 @@
     <v-row>
       <v-col cols="12">
         <Header text="About" />
+        <p>
+          <strong>
+            <span class="accent--text">SPOTIFY</span>
+            PARTY
+            <span class="accent--text">MUSIC</span>
+            PLANNER
+          </strong>
+          is an application that allows you to jointly plan playlists for social
+          gatherings. The application is integrated with Spotify's REST API.
+        </p>
+        <p>
+          It also team project created by IT students at
+          <strong>Poznan University of Technology.</strong>
+        </p>
+        <strong>Authors:</strong>
+        <ul>
+          <li>Wojciech Kasperski</li>
+          <li>Jakub Riegel</li>
+          <li>Gracjan Trawiński</li>
+          <li>Łukasz Wełnic</li>
+        </ul>
       </v-col>
     </v-row>
   </v-container>
