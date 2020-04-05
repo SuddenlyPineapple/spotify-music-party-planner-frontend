9052b5f15b2ecd488e6b40d10e32a7bc515b8dfe
Wojciech Kasperski <wojciech.kasperski97@gmail.com>
2020-04-05 23:30:03 +0200

    UserStory1- Extract event info to component

--- src/views/Event.vue
+++ src/views/Event.vue
@@ -8,83 +8,7 @@
     <v-row v-else>
       <v-col cols="12">
         <Header text="Event" />
-        <v-container cols="12" style="background-color: #2E2E2E; color:#FFF;">
-          <v-row>
-            <v-col style="margin:0; padding:0; padding-left:20px;">
-              <v-img
-                v-if="notBigEnough()"
-                contain
-                :src="getTrackThumbnail(0)"
-                cols="4"
-              />
-              <v-row v-else v-for="m in 2" :key="m" style="margin:0; padding:0">
-                <v-col
-                  v-for="n in 2"
-                  :key="n"
-                  style="margin:0; padding:0"
-                  md="auto"
-                >
-                  <v-img
-                    v-if="m == 1"
-                    :src="getTrackThumbnail(m + n - 2)"
-                    width="100"
-                  >
-                  </v-img>
-                  <v-img v-else :src="getTrackThumbnail(m + n - 1)" width="100">
-                  </v-img>
-                </v-col>
-              </v-row>
-            </v-col>
-            <v-divider vertical></v-divider>
-            <v-col cols="7">
-              <div
-                style="font-size:9px; letter-spacing: 10px; font-weight: 600;"
-              >
-                EVENT
-              </div>
-              <div
-                style="font-size:27px; font-weight: 900; letter-spacing: 2px;"
-              >
-                {{ event.name }}
-              </div>
-              <div
-                style="font-size:13px; letter-spacing: 3px; font-weight: 500; margin-top:-7px; margin-bottom:7px;"
-              >
-                Tracks: {{ tracks.length }}
-              </div>
-              <div
-                style="font-size:13px; letter-spacing: 3px; font-weight: 500;"
-              >
-                Location:
-              </div>
-              <div
-                style="font-size:15px; font-weight: 700; letter-spacing: 2px; margin-bottom:5px;"
-              >
-                {{ event.location }}
-              </div>
-              <div
-                style="font-size:13px; letter-spacing: 3px; font-weight: 500;"
-              >
-                Date:
-              </div>
-              <div
-                style="font-size:15px; font-weight: 700; letter-spacing: 2px; margin-bottom:5px;"
-              >
-                {{ event.date }}
-              </div>
-              <div
-                style="font-size:13px; letter-spacing: 3px; font-weight: 500;"
-              >
-                Host:
-              </div>
-              <div
-                style="font-size:15px; font-weight: 700; letter-spacing: 2px; margin-bottom:5px;"
-              >
-                {{ event.hostId }}
-              </div>
-            </v-col>
-          </v-row>
-        </v-container>
+        <EventInfo :event="event" :tracks="tracks" />
       </v-col>
       <v-col cols="12">
         <v-btn @click="$router.push('/search/' + id)" color="accent" dark>
@@ -120,6 +44,7 @@
 <script>
 import Header from "../components/Header";
 import SongList from "../components/SongList";
+import EventInfo from "../components/EventInfo";
 import DeleteEventModal from "../components/DeleteEventModal";
 import ErrorMessage from "../components/ErrorMessage";
 import axios from "axios";
@@ -138,7 +63,8 @@ export default {
     Header,
     SongList,
     DeleteEventModal,
-    ErrorMessage
+    ErrorMessage,
+    EventInfo
   },
   methods: {
     getEvent() {
@@ -163,18 +89,6 @@ export default {
         this.error =
           "There is a problem with Spotify API or our backend. Try refresh page or contact Administraor.";
       this.loading = false;
-    },
-    getTrackThumbnail(n) {
-      if (this.tracks.length != 0) {
-        if (this.tracks[n].album.img.length >= 2) {
-          console.log(this.tracks[n].album.img[1].url);
-          return this.tracks[n].album.img[1].url;
-        } else return this.tracks[n].album.img[0].url;
-      } else return "../assets/default-album-artwork.png";
-    },
-    notBigEnough() {
-      if (this.tracks.length < 4) return true;
-      else return false;
     }
   },
   mounted() {
